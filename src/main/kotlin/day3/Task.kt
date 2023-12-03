bfe50fb3882c908112ecc21e3508cc3e083794ce
Willdotwhite <willdotwhiteis@gmail.com>
2023-12-03 11:51:16 +0000

    Day 3 part 1

--- src/main/kotlin/day3/Task.kt
+++ src/main/kotlin/day3/Task.kt
@@ -1,9 +1,64 @@
 package day3
 
 import ITask
+import kotlin.math.abs
 
 class Task: ITask {
     override fun solve(input: List<String>): Int {
-        TODO("Not yet implemented")
+        val allNumberSpans = mutableListOf<NumberSpan>()
+        val allEngineSymbols = mutableListOf<EngineSymbol>()
+
+        // Convert List<String> which look something like "467..114.*" into a 2d grid of Char
+        val charGrid: Array<CharArray> = input
+            .map { line -> line.toCharArray() }
+            .toTypedArray()
+
+
+        for (y in charGrid.indices) {
+            val row = charGrid[y]
+            var currentNumberSpan: NumberSpan = mutableListOf()
+
+            for (x in row.indices) {
+                val cell = row[x]
+
+                if (cell.isDigit()) {
+                    currentNumberSpan.add(Pair(cell.digitToInt(), Vector2(x, y)))
+                    continue
+                }
+
+                // If we have a NumberSpan in progress, and we've hit a non-digit char,
+                // track that and then handle the new char
+                allNumberSpans.add(currentNumberSpan)
+                currentNumberSpan = mutableListOf()
+
+                if (cell != '.') {
+                    allEngineSymbols.add(Vector2(x, y))
+                }
+
+            }
+
+            // If we ended the row with a non-empty number span, save that before moving on
+            if (currentNumberSpan.isNotEmpty()) {
+                allNumberSpans.add(currentNumberSpan)
+            }
+        }
+
+        return allNumberSpans
+            .filter {  it.isAdjacentToAnySymbol(allEngineSymbols) }
+            .map { it.fold("") { acc, (value, _) -> acc + value } }
+            .sumOf { it.toInt() }
     }
+}
+
+data class Vector2(val x: Int, val y: Int)
+
+typealias EngineSymbol = Vector2
+typealias NumberSpan = MutableList<Pair<Int, Vector2>>
+
+fun NumberSpan.isAdjacentToAnySymbol(symbols: List<EngineSymbol>): Boolean {
+    return symbols.any { symbol -> this.isAdjacentToSymbol(symbol) }
+}
+fun NumberSpan.isAdjacentToSymbol(symbol: EngineSymbol): Boolean {
+    // There may be a way to optimise this, but we'll brute force for now
+    return this.any { (_, coords) -> abs(coords.x - symbol.x) <= 1 && abs(coords.y - symbol.y) <= 1 }
 }
--- src/test/kotlin/day3/TaskTest.kt
+++ src/test/kotlin/day3/TaskTest.kt
@@ -1,9 +1,57 @@
 package day3
 
 import ITaskTest
+import mapMultiLineString
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class TaskTest: ITaskTest {
+
+    private val testTask: Task = Task()
+
+    @Test
     override fun testExampleInput() {
-        TODO("Not yet implemented")
+        val input = mapMultiLineString("""
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...${'$'}.*....
+            .664.598..
+        """.trimIndent())
+
+        assertEquals(4361, testTask.solve(input))
+    }
+    @Test
+    fun testLiteralEdgeCase() {
+        val input = mapMultiLineString("""
+            467..114..
+            ...*......
+            ..35...633
+            .......#..
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
+        """.trimIndent())
+
+        assertEquals(4361, testTask.solve(input))
+    }
+
+    @Test
+    fun testRunOnSymbols() {
+        val input = mapMultiLineString("""
+            4^.....%2.
+            1%.....*4.
+            3".....2(..
+        """.trimIndent())
+
+        assertEquals(16, testTask.solve(input))
     }
 }
