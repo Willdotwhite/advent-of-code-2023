2d7781388ae7bb763b2717f9437cd9376c7ead83
WillDotWhite <willdotwhiteis@gmail.com>
2023-12-08 12:39:30 +0000

    Day 8 part 2

--- src/main/kotlin/day8/Task.kt
+++ src/main/kotlin/day8/Task.kt
@@ -1,9 +1,9 @@
 package day8
 
-import ITask
+import kotlin.math.max
 
-class Task: ITask {
-    override fun solve(input: List<String>): Int {
+class Task {
+    fun solve(input: List<String>): Long {
         val network = Network()
 
         val stepsToFollow = input[0]
@@ -15,23 +15,57 @@ class Task: ITask {
                 network.addNode(it)
             }
 
-        val startingNodeId = "AAA"
-        var totalSteps = 0
+
+        // Walk all paths until they loop; find multiplicative factor until they do
+        val nodeTravellers: List<NodeTraveller> = network.getStartingNodes()
+            .map { NodeTraveller(network, stepsToFollow, it) }
+
+        nodeTravellers.parallelStream().forEach { it.walkUntilAnExit() }
+
+        val numberOfStepsForNodesToGetToEnd = nodeTravellers.map { it.numberOfStepsInPath }.toList()
+        // 14935034899483
+        return findLCMOfListOfNumbers(numberOfStepsForNodesToGetToEnd)
+    }
+
+    private fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
+        var result = numbers[0]
+        for (i in 1 until numbers.size) {
+            result = findLCM(result, numbers[i])
+        }
+        return result
+    }
+
+    private fun findLCM(a: Long, b: Long): Long {
+        val larger = max(a, b)
+        val maxLcm = a * b
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm % a == 0.toLong() && lcm % b == 0.toLong()) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
+}
+
+class NodeTraveller(val network: Network, private val stepsToFollow: String, startingNode: Node) {
+    private var currentNode: Node = startingNode
+
+    var numberOfStepsInPath = 0L
+
+    fun walkUntilAnExit() {
         var stepIndex = 0
 
-        var currentNode = network.getNodeById(startingNodeId)
-        while (currentNode.id != "ZZZ") {
-            totalSteps += 1
-
+        while (!isOnFinishNode() || numberOfStepsInPath == 0L) {
+            numberOfStepsInPath += 1
             val nextNodeId = if (stepsToFollow[stepIndex] == 'L') currentNode.leftId else currentNode.rightId
-            stepIndex = (stepIndex + 1) % stepsToFollow.length // TODO: Verify this works properly
-
+            stepIndex = (stepIndex + 1) % stepsToFollow.length
             currentNode = network.getNodeById(nextNodeId)
         }
-
-
-        return totalSteps
     }
+
+    private fun isOnFinishNode() = currentNode.id.endsWith("Z")
 }
 
 typealias NodeId = String
@@ -45,4 +79,5 @@ class Network {
     }
 
     fun getNodeById(startingNodeId: String): Node = nodes[startingNodeId]!!
+    fun getStartingNodes(): List<Node> = nodes.entries.filter { it.key.endsWith("A") }.map { it.value }.toList()
 }
--- src/test/kotlin/day8/TaskTest.kt
+++ src/test/kotlin/day8/TaskTest.kt
@@ -12,16 +12,17 @@ class TaskTest: ITaskTest {
     @Test
     override fun testExampleInput() {
         val input = mapMultiLineString("""
-            RL
-            AAA = (BBB, CCC)
-            BBB = (DDD, EEE)
-            CCC = (ZZZ, GGG)
-            DDD = (DDD, DDD)
-            EEE = (EEE, EEE)
-            GGG = (GGG, GGG)
-            ZZZ = (ZZZ, ZZZ)
+            LR
+            11A = (11B, XXX)
+            11B = (XXX, 11Z)
+            11Z = (11B, XXX)
+            22A = (22B, XXX)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            XXX = (XXX, XXX)
         """.trimIndent())
 
-        assertEquals(2, task.solve(input))
+        assertEquals(6, task.solve(input))
     }
 }
